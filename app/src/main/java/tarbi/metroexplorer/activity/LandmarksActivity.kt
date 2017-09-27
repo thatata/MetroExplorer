037e35f04dd19fe386fa7d54d01dd15e31fbc856
Robert Gifford <robertgif@gmail.com>
2017-09-26 22:44:58 -0400

    Setup initial file and system architecture

---
+++ app/src/main/java/tarbi/metroexplorer/activity/LandmarksActivity.kt
@@ -0,0 +1,8 @@
+package tarbi.metroexplorer.activity
+
+/**
+ * Created by hobbes on 9/26/17.
+ */
+class LandmarksActivity {
+    /* TODO */
+}
---
+++ app/src/main/java/tarbi/metroexplorer/util/PersistanceManager.kt
@@ -0,0 +1,8 @@
+package tarbi.metroexplorer.util
+
+/**
+ * Created by hobbes on 9/26/17.
+ */
+class PersistanceManager {
+    /* TODO */
+}
