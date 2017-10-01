7f0d353bc369a8fb40c5dffe4b4dbdc27d36fe7d
Robert Gifford <robertgif@gmail.com>
2017-10-01 19:41:47 -0400

    Add buttons in main that go to propper activities

--- app/src/main/java/tarbi/metroexplorer/activity/MetroStationActivity.kt
+++ app/src/main/java/tarbi/metroexplorer/activity/MetroStationActivity.kt
@@ -1,8 +1,16 @@
 package tarbi.metroexplorer.activity
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import tarbi.metroexplorer.R
+
 /**
  * Created by hobbes on 9/26/17.
  */
-class MetroStationActivity {
-    /* TODO */
+class MetroStationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metrostation)
+    }
 }
