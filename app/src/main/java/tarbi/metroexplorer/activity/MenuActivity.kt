c574cf9885178865b8dcf1aacca704e756f86719
Robert Gifford <robertgif@gmail.com>
2017-10-03 12:00:57 -0400

    Remove unnecessary code

--- app/src/main/java/tarbi/metroexplorer/activity/MenuActivity.kt
+++ app/src/main/java/tarbi/metroexplorer/activity/MenuActivity.kt
@@ -3,7 +3,6 @@ package tarbi.metroexplorer.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.TextView
 import tarbi.metroexplorer.R
 
