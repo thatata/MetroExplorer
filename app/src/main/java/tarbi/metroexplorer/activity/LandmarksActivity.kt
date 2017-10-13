9c8fe51954f44d4fd8be31291e78b04af5dda43b
Robert Gifford <robertgif@gmail.com>
2017-10-13 15:53:19 -0400

    Initial implementation for receiving station info

--- app/src/main/java/tarbi/metroexplorer/activity/LandmarksActivity.kt
+++ app/src/main/java/tarbi/metroexplorer/activity/LandmarksActivity.kt
@@ -3,14 +3,21 @@ package tarbi.metroexplorer.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import tarbi.metroexplorer.R
+import tarbi.metroexplorer.util.FetchMetroStationsManager
+import tarbi.metroexplorer.util.Station
 
-/**
+/*
  * Created by hobbes on 9/26/17.
  */
 class LandmarksActivity : AppCompatActivity() {
 
      override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landmarks)
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_landmarks)
+
+         /* Use FetchMetroStationsManager to get closest station */
+         val stationManager = FetchMetroStationsManager(38.8978168, -77.0404246, 500.0,
+                 applicationContext)
+         val nearestStation: Station? = stationManager.getNearestStation()
     }
 }
